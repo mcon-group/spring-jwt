4fe2343ceaff33fbb73a6629a6937dcd4c5e96dd
ruben malchow <ruben.malchow@gmail.com>
2018-02-10 13:22:41 +0100

    removed excess modules

--- jwt-client/src/main/java/com/mcg/jwt/client/JwtInterceptor.java
+++
@@ -1,7 +0,0 @@
-package com.mcg.jwt.client;
-
-import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
-
-public class JwtInterceptor extends HandlerInterceptorAdapter {
-
-}
--- jwt-common/src/main/java/com/mcg/jwt/common/CryptoUtils.java
+++
@@ -1,5 +0,0 @@
-package com.mcg.jwt.common;
-
-public class CryptoUtils {
-
-}
