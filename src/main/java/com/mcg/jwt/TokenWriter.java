eb4cf015bc7a76e9027b21182ead2cdd4d6fbf67
Ruben Malchow <ruben.malchow@mcon-group.com>
2019-09-19 13:00:59 +0200

    select signing algorithm based on key length

--- src/main/java/com/mcg/jwt/TokenWriter.java
+++ src/main/java/com/mcg/jwt/TokenWriter.java
@@ -1,6 +1,7 @@
 package com.mcg.jwt;
 
 import java.security.NoSuchAlgorithmException;
+import java.security.interfaces.ECPrivateKey;
 import java.util.Date;
 import java.util.Map;
 
@@ -24,9 +25,17 @@ public abstract class TokenWriter<T> {
 		b = b.setExpiration(expires);
 		b = b.addClaims(map(in));
 		if(epk.getAlgorithm().equals("RSA")) {
-			b = b.signWith(SignatureAlgorithm.RS256, epk.getPrivateKey());
+			b = b.signWith(epk.getPrivateKey(),SignatureAlgorithm.RS256);
 		} else if(epk.getAlgorithm().equals("EC")) {
-			b = b.signWith(SignatureAlgorithm.ES256, epk.getPrivateKey());
+			ECPrivateKey ecpk = (ECPrivateKey) epk.getPrivateKey();
+			int bl = ecpk.getParams().getOrder().bitLength();
+			if(bl==384) {
+				b = b.signWith(epk.getPrivateKey(),SignatureAlgorithm.ES384);
+			} else if (bl==256) {
+				b = b.signWith(epk.getPrivateKey(),SignatureAlgorithm.ES256);
+			} else if (bl==512) {
+				b = b.signWith(epk.getPrivateKey(),SignatureAlgorithm.ES512);
+			}
 		} else {
 			throw new NoSuchAlgorithmException();
 		}
--- src/test/java/com/mcg/jwt/api/EncodeDecodeTest.java
+++ src/test/java/com/mcg/jwt/api/EncodeDecodeTest.java
@@ -1,12 +1,16 @@
 package com.mcg.jwt.api;
 
+import java.security.KeyFactory;
 import java.security.NoSuchAlgorithmException;
+import java.security.Signature;
+import java.security.spec.X509EncodedKeySpec;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.springframework.util.Base64Utils;
 
 import com.mcg.jwt.crypto.DefaultKeyProvider;
 import com.mcg.jwt.example.User;
@@ -39,9 +43,7 @@ public class EncodeDecodeTest {
 		u.setAuthorities(x);
 		
 		String s = utw.createToken(u, new Date(System.currentTimeMillis()+5000));
-		System.err.println(s);
 		User u2 = utr.readToken(s);
-		System.err.println(u2);
 		
 		Assert.assertEquals(u.getId(), u2.getId());
 		Assert.assertEquals(u.getName(), u2.getName());
@@ -102,12 +104,43 @@ public class EncodeDecodeTest {
 
 	}
 	
+	@Test
+	public void repeatEncodeDecodeTest() throws NoSuchAlgorithmException, TokenException {
+		for(int i=0;i < 1000;i++) {
+			try {
+				testEncodeDecodeEC();
+				testEncodeDecodeRSA();
+			} catch (Exception e) {
+				throw e;
+			}
+		}
+		
+	}
+	
 	@Test(expected=NoSuchAlgorithmException.class)
 	public void testEncodeDecodeRandom() throws NoSuchAlgorithmException, TokenException {
 		DefaultKeyProvider dkp = new DefaultKeyProvider();
 		dkp.setAlgorithm("HUND");
 		dkp.generateKeyPair();
 	}
+	
+	public static void main(String[] args) throws Exception {
+		String PubKey= "MHYwEAYHKoZIzj0CAQYFK4EEACIDYgAE2sNRGKd0NvYaCXISCT5Fq+ImFsjQFsLqJemw6qmmXU2QI1DLq0MmmhcYWWDJeyrfRPNiFivvH2J5bWyNotPCDVXhd9zgIkp7efv7Ryy8vxwcX34L+GRBAKOlPU0NdH4/";
+		String Data = "ZXlKelpYSnBZV3dpT2lJeE5UWTRPRGc1TWpNMk16TTJJaXdpWVd4bklqb2lSVk15TlRZaWZRLmV5SmxlSEFpT2pFMU5qZzRPRGt5TkRZc0ltNWhiV1VpT2lKcllYUjZaU0lzSW1sa0lqb2lhSFZ1WkNJc0ltRjFkR2h2Y21sMGFXVnpJanBiSW1FaUxDSmlJaXdpWXlKZGZR";
+		String Sig = "MGQCMDA2XaOtkfK/ukG8DXcNMjl7EvaUNF46sOPM2WnFsdaY3iQ0I6W7wSVlC9uGpjTOvQIwGrHkYTfgPmwIyOSDuuFP5Fvc2ljtE++Gn17tpxd44WDvAsb7GiaBNsk8W8/Pybs3";
+		
+		KeyFactory kf = KeyFactory.getInstance("EC");
+
+		// Decode the private key (read as a byte[] called 'buf').
+		X509EncodedKeySpec ks = new X509EncodedKeySpec(Base64Utils.decodeFromString(PubKey));
+		
+		Signature s = Signature.getInstance("SHA256withECDSA");
+		s.initVerify(kf.generatePublic(ks));
+		s.update(Base64Utils.decodeFromString(Data));
+		System.err.println(s.verify(Base64Utils.decodeFromString(Sig)));
+		
+	}
+	
 	
 
 
